9a8789ebb42267d1b5181b0c5f0b2fc2c97bb264
WilliamCappelletti <cappelletti.william@gmail.com>
2019-12-09 16:40:48 +0100

    implement kernel prediction

--- src/utility/kernel.py
+++ src/utility/kernel.py
@@ -1,52 +1,87 @@
 import torch
-
-# from .util import one_hot
-# import copy
-
-# def tangentKernel(network, input):
-#     ker = torch.Tensor()
-#     pass
-
-# def newPointKernel(x, network, input):
-#     pass
+import time
 
 class NeuralTangentKernel:
-    def __init__(self, X, net, device=None):
+    def __init__(self, X, net, device=None, verbose=False):
         if device is None:
             self.device = 'cuda' if torch.cuda.is_available() else 'cpu'
         else:
             self.device = device
+        
+        self.net = net.to(device)
 
-        net = net.to(self.device)
+        self.n_samples = X.shape[0]
+        self.dim_out = list(net.modules())[-1].out_features
+        self.n_params = sum(par.numel() for par in net.parameters() if par.requires_grad)
+
+        start_time = time.time()
+
+        self.Jac = self.compute_jacobians(X)
+
+        if verbose: print("Computed jacobians in {}s".format(time.time() - start_time))
+                    
+    
+    def kernel(self):
+        # compute the kernel value
+        return torch.einsum('abp, cdp -> abcd', self.Jac, self.Jac).reshape(self.n_samples*self.dim_out, self.n_samples*self.dim_out)
+
+    def compute_jacobians(self, X):
+        """Returns the jacobians of the neural network for each parameter and each input.
+        
+        Parameters
+        ----------
+        X : tensor
+            Inputs for the network
+        
+        Returns
+        -------
+        tensor
+            The jacobian encoded as a tensor of size (n_samples, dim_out, n_param)
+        """
         X = X.to(self.device)
 
-        net.zero_grad()
-        out = net(X)
+        self.net.zero_grad()
+        out = self.net(X)
 
-        n_samples, dim_out = out.shape
-        n_params = sum(par.numel() for par in net.parameters() if par.requires_grad)
+        Jac = torch.zeros(self.n_samples, self.dim_out, self.n_params)
 
-    #     kernel = torch.zeros([n_samples * dim_out] * 2, dtype=torch.float).to(device)
-        
         with torch.no_grad():
-            # compute the jacobians
-            self.Jac = torch.zeros(n_samples, dim_out, n_params)
-            for x in range(n_samples):
-                for i in range(dim_out):
+            for x in range(self.n_samples):
+                for i in range(self.dim_out):
                     direction = torch.zeros_like(out, dtype=torch.float)
                     direction[x, i] = 1.
                     # Note, if we manage to flatten in one tensor each dic entry, we can use it a tensor as well.
-                    JacList = torch.autograd.grad(out, net.parameters(), direction, retain_graph=True)
+                    JacList = torch.autograd.grad(out, self.net.parameters(), direction, retain_graph=True)
                     
-                    self.Jac[x, i] = torch.cat(tuple(map(lambda par: par.reshape(-1) , JacList)))
-                    
-                    
-            # compute the kernel value
-            self.kernel = torch.einsum('abp, cdp -> abcd', self.Jac, self.Jac).reshape(n_samples*dim_out, n_samples*dim_out)
-    
+                    Jac[x, i] = torch.cat(tuple(map(lambda par: par.reshape(-1) , JacList)))
+        return Jac
+        
+
     def regression_exact(self, input_train, target_train):
-        self.coefficients = torch.cholesky_solve(target_train, self.kernel.unsqueeze(0).to(self.device))
+        """Compute the kernel regression coefficients by inverting the kernel.
+        
+        Parameters
+        ----------
+        input_train : tensor
+        target_train : tensor
+        
+        Returns
+        -------
+        self
+        """
+        self.coefficients = torch.cholesky_solve(target_train, self.kernel().unsqueeze(0).to(self.device))
         return self
 
-    def prediction_exact(self, input_test):
-        pass
+    def predict(self, input_test):
+        """Compute the predicted output using the previously computed coefficients.
+        
+        Parameters
+        ----------
+        input_test : tensor
+        """
+        n_test = input_test.shape[0]
+        Jac_te = self.compute_jacobians(input_test)
+
+        kernel_te = torch.einsum('abp, cdp -> abcd', Jac_te, self.Jac).reshape(n_test * self.dim_out, self.n_samples * self.dim_out)
+        
+        return kernel_te @ self.coefficients
