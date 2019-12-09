a7a89578d58597967cf8973e76316ce0514fc2a5
WilliamCappelletti <cappelletti.william@gmail.com>
2019-12-09 16:38:06 +0100

    use cuda in 1-hot encoding

--- src/utility/util.py
+++ src/utility/util.py
@@ -3,4 +3,5 @@ import torch
 def one_hot(batch,depth):
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
     ones = torch.sparse.torch.eye(depth)
+    ones = ones.to(device)
     return ones.index_select(0,batch)
